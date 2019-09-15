7721c6411d871090f41f5ab02387cc1e46a78f6f
Porfirio Matias <porfirio.matias@meltwater.com>
2019-09-14 21:56:03 -0400

    happy path test for lambda

--- src/index.js
+++ src/index.js
@@ -7,44 +7,44 @@ import google from 'googleapis';
 AWS.config.setPromisesDependency(Promise);
 AWS.config.update({region: process.env.AWS_REGION});
 
-const GOOGLE_AUTH_CLIENT_SCOPE = [
-    'https://www.googleapis.com/auth/youtube.upload',
-    'https://www.googleapis.com/auth/youtube.readonly'
-];
-const googleAuthClient = new GoogleAuthClient({
-    google
-});
+// const GOOGLE_AUTH_CLIENT_SCOPE = [
+//     'https://www.googleapis.com/auth/youtube.upload',
+//     'https://www.googleapis.com/auth/youtube.readonly'
+// ];
+// const googleAuthClient = new GoogleAuthClient({
+//     google
+// });
 
 const s3 = new AWS.S3();
 
-async function consume(event, context) {
-    await googleAuthClient.authenticate(GOOGLE_AUTH_CLIENT_SCOPE);
-    const youtube = new Youtube({
-        google,
-        googleAuthClient
-    });
-    const bucket = event.Records[0].s3.bucket.name;
-    const fileKey = event.Records[0].s3.object.key;
-    const params = {
-        Bucket: bucket,
-        Key: fileKey
-    };
+// async function consume(event, context) {
+//     await googleAuthClient.authenticate(GOOGLE_AUTH_CLIENT_SCOPE);
+//     const youtube = new Youtube({
+//         google,
+//         googleAuthClient
+//     });
+//     const bucket = event.Records[0].s3.bucket.name;
+//     const fileKey = event.Records[0].s3.object.key;
+//     const params = {
+//         Bucket: bucket,
+//         Key: fileKey
+//     };
 
-    let video;
+//     let video;
 
-    try {
-        video = await s3.getObject(params).promise();
-    } catch(error) {
-        throw new Error(`An error occurred while downloading data from S3 in the Youtube Uploader Lambda: ${error.message}`);
-    }
+//     try {
+//         video = await s3.getObject(params).promise();
+//     } catch(error) {
+//         throw new Error(`An error occurred while downloading data from S3 in the Youtube Uploader Lambda: ${error.message}`);
+//     }
 
-    try {
-        await youtube.upload(video)
-    } catch (error) {
-        throw new Error(`An error occurred while upload video data to Youtube in the Youtube Uploader Lambda: ${error.message}`);
-    }
-    return {};
-}
+//     try {
+//         await youtube.upload(video)
+//     } catch (error) {
+//         throw new Error(`An error occurred while upload video data to Youtube in the Youtube Uploader Lambda: ${error.message}`);
+//     }
+//     return {};
+// }
 
 async function test() {
     return { message: 'Hey I worked!'};
