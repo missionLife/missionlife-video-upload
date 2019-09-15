ebe6d1dcd08dcc584bf901d90ced0b30bf73c6db
Porfirio Matias <porfirio.matias@meltwater.com>
2019-09-14 21:51:02 -0400

    added open to node modules

---
+++ src/google-auth-client/index.spec.js
@@ -0,0 +1,7 @@
+import GoogleAuthClient from './index';
+
+describe('GoogleAuthClient', () => {
+    it('should exist', () => {
+        expect(GoogleAuthClient).toEqual(jasmine.any(Function));
+    });
+})
