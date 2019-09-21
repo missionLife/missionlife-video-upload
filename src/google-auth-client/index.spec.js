7399ddccdd84fdf0dc6439912f0acfebafeee00a
missionLife <SSJ6Porfy@hotmail.com>
2019-09-20 23:25:00 -0400

    changed src folder

--- src/google-auth-client/index.spec.js
+++
@@ -1,7 +0,0 @@
-import GoogleAuthClient from './index';
-
-describe('GoogleAuthClient', () => {
-    it('should exist', () => {
-        expect(GoogleAuthClient).toEqual(jasmine.any(Function));
-    });
-})
--- src/youtube/index.spec.js
+++
@@ -1,7 +0,0 @@
-import Youtube from './index';
-
-describe('Youtube', () => {
-    it('should exist', () => {
-        expect(Youtube).toEqual(jasmine.any(Function));
-    });
-})
